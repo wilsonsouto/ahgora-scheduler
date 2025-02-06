7f06ab9237686a23838be415b81babe314527722
Wilson Souto <wilsonsouto@outlook.com.br>
2025-02-06 11:34:35 -0300

    Refactor AhgoraService: move to new file and update project structure

---
+++ AhgoraScheduler/Services/AhgoraService.cs
@@ -0,0 +1,65 @@
+using AhgoraScheduler.Models;
+using DotNetEnv;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AhgoraScheduler.Services;
+
+public class AhgoraService
+{
+    public static void Run()
+    {
+        Console.Clear();
+        var env = GetCredencials();
+
+        if (env == null)
+            return;
+
+        using var driver = new ChromeDriver();
+        driver.Navigate().GoToUrl(env.SiteUrl);
+
+        // Wait 10 seconds before search for an element
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+        IWebElement rootButton = driver.FindElement(By.XPath("//button[@type='button' and contains(@class, 'MuiButton-root')]"));
+        rootButton.Click();
+
+        IWebElement registrationField = driver.FindElement(By.Id("outlined-basic-account"));
+        registrationField.SendKeys(env.UserRegistration);
+
+        IWebElement passwordField = driver.FindElement(By.Id("outlined-basic-password"));
+        passwordField.SendKeys(env.UserPassword);
+
+        // IWebElement advanceButton = driver.FindElement(By.XPath("//button[.//p[text()='Advance']]"));
+        // advanceButton.Click();
+
+        Console.WriteLine("Your point has been registered!");
+    }
+
+    private static AhgoraModel? GetCredencials()
+    {
+        try
+        {
+            Env.Load();
+            var siteUrl = Environment.GetEnvironmentVariable("SITE_URL");
+            var userRegistration = Environment.GetEnvironmentVariable("USER_REGISTRATION");
+            var userPassword = Environment.GetEnvironmentVariable("USER_PASSWORD");
+
+            if (string.IsNullOrEmpty(siteUrl))
+                throw new Exception("Error: The 'SITE_URL' is missing or empty! Please check your .env file.");
+
+            if (string.IsNullOrEmpty(userRegistration))
+                throw new Exception("Error: The 'USER_REGISTRATION' is missing or empty! Please check your .env file.");
+
+            if (string.IsNullOrEmpty(userPassword))
+                throw new Exception("Error: The 'USER_PASSWORD' is missing or empty! Please check your .env file.");
+
+            return new AhgoraModel(siteUrl, userRegistration, userPassword);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return null;
+        }
+    }
+}
